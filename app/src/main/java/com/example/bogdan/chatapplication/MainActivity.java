adf0fe1d64c15c6d814bd7a70892be97ceb35d12
boltean92 <boltean92@gmail.com>
2017-01-19 19:48:01 +0200

    Project refactoring and clean up

--- app/src/main/java/com/example/bogdan/chatapplication/MainActivity.java
+++ app/src/main/java/com/example/bogdan/chatapplication/MainActivity.java
@@ -1,13 +1,103 @@
 package com.example.bogdan.chatapplication;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ListView;
+
+import com.google.gson.Gson;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+
+import io.socket.client.IO;
+import io.socket.client.Socket;
+import io.socket.emitter.Emitter;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText mInputMessageView;
+    private MessageAdapter messageAdapter;
+    private String socketId;
+    private Button sendButton;
+    private Socket socket;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try {
+            socket = IO.socket("http://192.168.0.104:3000");
+            socket.connect();
+            Log.i("SOCLEEEELT", socket.toString());
+            socket.on("message", handleIncomingMessages);
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+        socketId = socket.id();
+
+        this.sendButton = (Button) findViewById(R.id.button);
+        this.mInputMessageView = (EditText) findViewById(R.id.editText);
+        ListView messagesView = (ListView) findViewById(R.id.messages_view);
+
+        this.messageAdapter = new MessageAdapter(this, new ArrayList<Message>());
+        messagesView.setAdapter(messageAdapter);
+        this.sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendMessage();
+            }
+        });
+    }
+
+    private void sendMessage() {
+        String message = mInputMessageView.getText().toString().trim();
+        mInputMessageView.setText("");
+        JSONObject sendText = new JSONObject();
+        try {
+            sendText.put("text", message);
+            sendText.put("user", "Bogdan");
+            socket.emit("message", sendText);
+        } catch (JSONException e) {
+
+        }
+
+    }
+
+
+    private Emitter.Listener handleIncomingMessages = new Emitter.Listener() {
+        @Override
+        public void call(final Object... args) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Gson gson = new Gson();
+                    JSONObject data = (JSONObject) args[0];
+                    Log.i("SOCLEEEELT", data.toString());
+                    Log.i("SOCLEEEELT", "in here");
+                    //needs some checks
+                    Message message = gson.fromJson(data.toString(), Message.class);
+                    Log.i("SOCLEEEELT", "User is: " + message.user);
+                    Log.i("SOCLEEEELT", "User is: " + message.text);
+                    messageAdapter.add(message);
+                }
+            });
+        }
+    };
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        socket.disconnect();
     }
 }
--- app/src/main/java/com/example/bogdan/chatapplication/MessageAdapter.java
+++ app/src/main/java/com/example/bogdan/chatapplication/MessageAdapter.java
@@ -4,62 +4,79 @@ package com.example.bogdan.chatapplication;
  * Created by Bogdan on 1/16/2017.
  */
 
+import android.app.Activity;
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.TextView;
 
 import java.util.List;
 
 
-public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.ViewHolder> {
+public class MessageAdapter extends BaseAdapter {
 
-    private List<Message> mMessages;
+    private List<Message> messageList;
+    Context messageContext;
+    private boolean isSelf;
 
     public MessageAdapter(Context context, List<Message> messages) {
-        mMessages = messages;
+        this.messageList = messages;
+        this.messageContext = context;
     }
 
     @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        int layout = -1;
-
-        View v = LayoutInflater
-                .from(parent.getContext())
-                .inflate(R.layout.layout_message, parent, false);
-        return new ViewHolder(v);
+    public int getCount() {
+        return messageList.size();
     }
 
     @Override
-    public void onBindViewHolder(ViewHolder viewHolder, int position) {
-        Message message = mMessages.get(position);
-        viewHolder.setMessage(message.getMessage());
+    public Object getItem(int index) {
+        return messageList.get(index);
     }
 
     @Override
-    public int getItemCount() {
-        return mMessages.size();
+    public long getItemId(int index) {
+        return index;
     }
 
     @Override
-    public int getItemViewType(int position) {
-        return mMessages.get(position).getType();
-    }
+    public View getView(int index, View view, ViewGroup viewGroup) {
+        MessageViewHolder holder;
+        if (view == null) {
+            LayoutInflater messageInflater = (LayoutInflater) messageContext.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
-        private TextView mMessageView;
+            view = messageInflater.inflate(R.layout.message_layout, null);
+            holder = new MessageViewHolder();
+            holder.senderView = (TextView) view.findViewById(R.id.message_sender);
+            holder.bodyView = (TextView) view.findViewById(R.id.message_body);
 
-        public ViewHolder(View itemView) {
-            super(itemView);
-            mMessageView = (TextView) itemView.findViewById(R.id.message);
+            view.setTag(holder);
+        } else {
+            holder = (MessageViewHolder) view.getTag();
         }
 
-        public void setMessage(String message) {
-            if (null == mMessageView) return;
-            mMessageView.setText(message);
-        }
+        Message message = (Message) getItem(index);
+        holder.bodyView.setText(message.text);
+        holder.senderView.setText(message.user);
 
+
+        return view;
+    }
+
+    public void isSelf(boolean isSelf) {
+        this.isSelf = isSelf;
+    }
+
+    private static class MessageViewHolder {
+        public TextView senderView;
+        public TextView bodyView;
+    }
+
+    public void add(Message message) {
+        messageList.add(message);
+        notifyDataSetChanged();
     }
 }
